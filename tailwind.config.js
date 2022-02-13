a90c1541550cafd4fb82a3044ec8c8386745232a
Robin Boers <robindev2019@outlook.com>
2022-02-13 17:52:14 +0000

    Update themes

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,46 +7,38 @@ module.exports = {
 
             // Trying
 
-            // black: "#1d0f44",
-            // white: "#f44e38",
-
-            // vibrant: "#00ffae",
-
             // Real options
 
             // black: "#222323",
             // white: "#f0f0f6",
             // vibrant: " #ff5730",
 
-            // very good on light mode
-            // white: "#ffffd9",
-            // black: "#00334d",
-            // vibrant: "#bf0a0d",
+            // black: "#7b4255",
+            // white: "#e6d3b9",
+            // vibrant: "#ea9971",
+
+            // black: "#253657",
+            // white: "#e6d3b9",
+            // vibrant: "#e76d8a",
+
+            // black: "#131324",
+            // white: "#f5fff3",
+            // vibrant: "#5ab9a8",
 
             // black: "#314e52",
             // white: "#f3f3f3",
             // vibrant: "#f2a154",
 
-            // black: "#272946",
-            // white: "#e7ffee",
-            // vibrant: "#eda031",
-            black: "#0d2b45", // better version
+            black: "#0d2b45",
             white: "#ffecd6",
             vibrant: "#ffaa5e",
 
-            // TO BLAND!!
+            // HMMM
 
-            // black: "#191818",
-            // white: "#d4c8bb",
-            // vibrant: "#7b60a6",
-
-            // black: "#262420",
-            // white: "#fcfffb",
-            // vibrant: "#215fa3",
-
-            // black: "#372a51",
-            // white: "#f5f6df",
-            // vibrant: "#5a8f78",
+            // very good on light mode, but dark mode meh
+            // white: "#ffffd9",
+            // black: "#00334d",
+            // vibrant: "#bf0a0d",
 
             transparent: "transparent",
             current: "currentColor",
