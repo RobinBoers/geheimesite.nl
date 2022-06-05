d5351d02ddf2128f8da1081834f70b728d92f16c
Robin Boers <robindev2019@outlook.com>
2022-06-05 17:00:39 +0200

    Add /me page

---
+++ src/me.php
@@ -0,0 +1,175 @@
+<!DOCTYPE html>
+<html lang="nl">
+    <head>
+        <meta charset="UTF-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Too much information about me — geheimesite.nl | Robin Boers</title>
+
+        <link rel="alternate stylesheet" href="/assets/css/plain.css" video-title="plain" />       
+        <script src="/assets/js/plain-mode.js"></script>
+
+        <link rel="stylesheet" href="/assets/css/main.css" video-title="default" />
+
+        <link 
+            rel="stylesheet" 
+            href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" 
+            crossorigin="anonymous" 
+            referrerpolicy="no-referrer" 
+        />
+
+        <style>
+            :root {
+                --black: 19, 19, 36;
+                --white: 243, 248, 255;
+                --vibrant: 36, 185, 196;
+            }
+
+            .fa-solid {
+                min-width: 20px;
+            }
+
+            li a {
+                line-height: revert;
+            }
+        </style>
+    </head>
+    <body>
+        <a class="skip-nav" href="#content">Skip navigation</a>
+        <header class="h-20 flex items-center pl-12">
+            <nav>
+                <a href="/en/">❮ Back</a>
+            </div>
+        </header>
+
+        <main id="content" class="flex-grow">
+            <article class="h-card max-w-[1200px] lg:flex flex-row gap-20">
+                <section>
+                    <img src="/assets/images/me.jpeg" class="u-photo w-full">
+                    <img src="/assets/images/robin_pixelart.png" hidden class="u-logo w-full pixelart">
+                </section>
+                <section>
+                    <h2 class="p-name mb-1 font-poppins">
+                        <span class="p-given-name">Robin</span> 
+                        <span class="p-family-name">Boers</span>
+                    </h2>
+                    <p class="italic">@<span class="p-nickname lowercase">Robijntje</span></p>
+
+                    <p class="p-note">
+                        I'm webdesigner, programmer, YouTuber, and teenager, and my hobbies are programming, video-editing, handball, and drawing.
+                        I really enjoy making websites and webapps, and I love to thinker with Linux.
+                    </p>
+
+                    <ul class="p-0 list-none">
+                        <li class="flex items-center gap-2 pt-1">
+                            <i class="fa-solid fa-earth-americas"></i> 
+                            <a rel="me" href="https://geheimesite.nl" class="u-url">geheimesite.nl</a>
+                        </li>
+                        <li class="flex items-center gap-2 pt-1">
+                            <i class="fa-solid fa-envelope"></i> 
+                            <a rel="me" href="mailto:robin@geheimesite.nl" class="u-email">robin@geheimesite.nl</a>
+                        </li>
+                        <li class="flex items-center gap-2 pt-1">
+                            <i class="fa-solid fa-paragraph"></i>
+                            <a href="https://blog.geheimesite.nl">blog.geheimesite.nl</a>
+                        </li>
+                        <li class="flex items-center gap-2 pt-1">
+                            <i class="fa-solid fa-cheese"></i>
+                            <time class="dt-anniversary dt-bday">2007-01-13</time>
+                        </li>
+                        <li class="flex items-center gap-2 pt-1">
+                            <i class="fa-solid fa-mars"></i>
+                            <span class="p-sex">M</span>
+                        </li>
+                        <li class="flex items-start gap-2 pt-1">
+                            <i class="fa-solid fa-building mt-1.5"></i>
+                            <span>
+                                <span class="p-job-title">Junior Developer</span>
+                                at <span class="p-org">Qdentity</span>
+                            </span>
+                        </li>
+                        
+                        <li class="flex items-top gap-2 pt-1">
+                            <i class="fa-solid fa-house  mt-1.5"></i>
+                            <span>
+                                <span class="p-locality">Maassluis</span> <br>
+                                <span class="p-region">Zuid-Holland</span>, <br>
+                                <span class="p-country-name">Netherlands</span>
+                            </span>
+                        </li>
+                    </ul>
+                </section>
+                <section>
+                    <h3>Elsewhere</h3>
+
+                    <ul class="p-0 list-none">
+                        <li>
+                            <a class="no-underline hover:bg-transparent hover:opacity-80" href="https://www.youtube.com/channel/UCx4li1iMygs5KtqgcU5KGRw" target="_blank" rel="me">
+                                <img src="https://img.shields.io/badge/Robin%20Boers-red?style=for-the-badge&logo=youtube" alt="Robin Boers on YouTube" />
+                            </a>
+                        </li>
+                        <li>
+                            <a class="no-underline hover:bg-transparent hover:opacity-80" href="https://github.com/RobinBoers" target="_blank" rel="me">
+                                <img src="https://img.shields.io/badge/GitHub-333333?style=for-the-badge&logo=github" alt="RobinBoers on GitHub" />
+                            </a>
+                        </li>
+                        <li>
+                            <a class="no-underline hover:bg-transparent hover:opacity-80" href="https://discord.gg/DPtKK3UREu" target="_blank" rel="me">
+                                <img src="https://img.shields.io/discord/712265706132996210?color=7289DA&label=%20&logo=discord&logoColor=white&style=for-the-badge" alt="Invite for Robin.io on Discord" />
+                            </a>
+                        </li>
+                        <li>
+                            <a class="no-underline hover:bg-transparent hover:opacity-80" href="https://www.linkedin.com/in/robin-boers-b18691229/" target="_blank" rel="me">
+                                <img src="https://img.shields.io/badge/linkedin-0A66C2?style=for-the-badge&logo=linkedin" alt="robinboers on LinkedIn" />
+                            </a>
+                        </li>
+                        <li>
+                            <a class="no-underline hover:bg-transparent hover:opacity-80" href="https://micro.blog/Robijntje" target="_blank" rel="me">
+                                <img src="https://img.shields.io/badge/robijntje-ff8800?style=for-the-badge&logo=micro.blog&logoColor=white" alt="@Robijntje on Micro.blog" />
+                            </a>
+                        </li>
+                        <li>
+                            <a class="no-underline hover:bg-transparent hover:opacity-80" href="https://www.twitch.tv/Rob1jntje" target="_blank" rel="me">
+                                <img src="https://img.shields.io/badge/Rob1jntje-blueviolet?style=for-the-badge&logo=twitch&logoColor=white" alt="Rob1jntje on Twitch.tv" />
+                            </a>
+                        </li>
+                        <li>
+                            <a class="no-underline hover:bg-transparent hover:opacity-80" href="https://robijntje.itch.io" target="_blank" rel="me">
+                                <img src="https://img.shields.io/badge/Rob1jntje-FA5C5C?style=for-the-badge&logoColor=white&logo=itch.io" alt="Robijntje on Itch.io" />
+                            </a>
+                        </li>
+                        <li>
+                            <a class="no-underline hover:bg-transparent hover:opacity-80" href="https://reddit.com/u/Robijntje22" target="_blank" rel="me">
+                                <img src="https://img.shields.io/badge/-u%2FRobijntje22-%23FF4500?style=for-the-badge&logoColor=white&logo=reddit" alt="Robijntje22 on Reddit" />
+                            </a>
+                        </li>
+                        <li>
+                            <a class="no-underline hover:bg-transparent hover:opacity-80" href="https://steamcommunity.com/id/Bobijntje/" target="_blank" rel="me">
+                                <img src="https://img.shields.io/badge/Gamertag1123456-1a293a?style=for-the-badge&logo=steam&logoColor=c7d5e0" alt="Gamertag1123456 on Steam" />
+                            </a>
+                        </li>
+                        <li>
+                            <a class="no-underline hover:bg-transparent hover:opacity-80" href="https://account.xbox.com/nl-nl/profile?gamertag=Robijntje22" target="_blank" rel="me">
+                                <img src="https://img.shields.io/badge/Robijntje22-107c10?style=for-the-badge&logo=xbox&logoColor=white" alt="Robijntje22 on Xbox Live" />
+                            </a>
+                        </li>
+                        <li>
+                            <a class="no-underline hover:bg-transparent hover:opacity-80" href="https://stackoverflow.com/users/12782055/robinboers" target="_blank" rel="me">
+                                <img src="https://img.shields.io/badge/StackOverflow-f8f9f9?style=for-the-badge&logo=stackoverflow&logoColor=f48225" alt="RobinBoers on Stackoverflow" />
+                            </a>
+                        </li>
+                        <li>
+                            <a class="no-underline hover:bg-transparent">
+                                <img src="https://img.shields.io/badge/Gamertag1123456-202124?style=for-the-badge&logo=epicgames&logoColor=white" alt="Gamertag1123456 on Epic Games" />
+                            </a>
+                        </li>
+                    </ul>
+                </section>
+            </article>
+        </main>
+
+        <footer class="page-footer">
+            &copy; <?php echo date("Y"); ?> Robin Boers. Webdesigner and Elixir programmer since 2017.
+        </footer>
+    </body>
+</html>
