18f866a6c4e28d055951b994cf80f94e39e00f0f
Robin Boers <robindev2019@outlook.com>
2022-06-06 15:52:45 +0200

    Add music page

---
+++ src/music.php
@@ -0,0 +1,228 @@
+<!DOCTYPE html>
+<html lang="nl">
+    <head>
+        <meta charset="UTF-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Music: my favorite tracks — geheimesite.nl | Robin Boers</title>
+
+        <link rel="alternate stylesheet" href="/assets/css/plain.css" title="plain" />       
+        <script src="/assets/js/plain-mode.js"></script>
+        
+        <link rel="stylesheet" href="/assets/css/main.css" title="default" />
+        <script defer src="/assets/js/menu.js"></script>
+
+        <style>
+            :root {
+                --black: 34, 35, 35;
+                --white: 240, 240, 246;
+                --vibrant: 40, 213, 118;
+            }
+            
+            html {
+                background: 
+                    linear-gradient(to right, lightskyblue, lime),
+                    url("/assets/images/content/cassettes.jpg");
+                background-blend-mode: hard-light, normal;
+                background-position: center;
+                background-size: 120%;
+            }
+
+            body {
+                background: 
+                    linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3));
+            }
+        </style>
+    </head>
+    <body class="text-white h-full">
+        <a class="skip-nav" href="#content">Skip navigation</a>
+        <header class="flex flex-col sm:flex-row-reverse items-center justify-around">
+            <p class="title"><a href="/">Robin Boers</a></p>
+
+            <nav>
+                <a href="/en/" class="text-white">❮ Back</a>
+            </div>
+        </header>
+
+        <main id="content" class="page-content text-black dark:text-white bg-white dark:bg-black pt-[50px] pb-[27px] border-x-0 border-solid border-t-[7px] border-vibrant max-w-full">
+            <article>
+                <section>
+                    <h1>Music</h1>
+
+                    <p>
+                        These are my favorite tracks ever. <a href="https://open.spotify.com/playlist/6vsurOP4dQ6ViTeOegLwgl?si=e0a3a51c6a8843bc">Stream 'em on Spotify</a>. Download 'em as using <a href="https://github.com/spotdl/spotify-downloader">spotdl</a>.
+                    </p>
+                </section>
+
+                <hr class="my-5 border-none">
+
+                <h2 hidden>
+                    cool underrated garbage
+                </h2>
+
+                <ol class="music-list">
+                    <li>
+                        <div>
+                            <h3 class="track-title">Afraid of the dark</h3>
+                            <p class="track-artist">Chef'Special</p>
+                        </div>
+                    </li>
+                    <li>
+                        <div>
+                            <h3 class="track-title">Demons</h3>
+                            <p class="track-artist">Imagine Dragons</p>
+                        </div>
+                    </li>
+                    <li>
+                        <div>
+                            <h3 class="track-title">Symphony</h3>
+                            <p class="track-artist">Clean Bandit <span class="supplement">&</span> Zara Larsson</p>
+                        </div>
+                    </li>
+                    <li>
+                        <div>
+                            <h3 class="track-title">All I Wanna Do</h3>
+                            <p class="track-artist">Martin Jensen</p>
+                        </div>
+                    </li>
+                    <li>
+                        <div>
+                            <h3 class="track-title">Shower</h3>
+                            <p class="track-artist">Becky G</p>
+                        </div>
+                    </li>
+                    <li>
+                        <div>
+                            <h3 class="track-title new">Impossible</h3>
+                            <p class="track-artist">James Arthur</p>
+                        </div>
+                    </li>
+                    <li>
+                        <div>
+                            <h3 class="track-title">Cover Me In Sunshine</h3>
+                            <p class="track-artist">P!nk <span class="supplement">&</span> Willow Sage Hart</p>
+                        </div>
+                    </li>
+                    <li>
+                        <div>
+                            <h3 class="track-title new">Zij Wil Mij</h3>
+                            <p class="track-artist">FLEMMING</p>
+                        </div>
+                    </li>
+                    <li>
+                        <div>
+                            <h3 class="track-title new">Vluchtstrook</h3>
+                            <p class="track-artist">Kris Kross Amsterdam <span class="supplement">&</span> Antoon <span class="supplement">&</span> Sigourney K</p>
+                        </div>
+                    </li>
+                    <li>
+                        <div>
+                            <h3 class="track-title">The Ocean</h3>
+                            <p class="track-artist">Mike Perry <span class="supplement">&</span> SHY Martin</p>
+                        </div>
+                    </li>
+                    <li>
+                        <div>
+                            <h3 class="track-title">abcdefu</h3>
+                            <p class="track-artist">GAYLE</p>
+                        </div>
+                    </li>
+                    <li>
+                        <div>
+                            <h3 class="track-title">traitor</h3>
+                            <p class="track-artist">Olivia Rodrigo</p>
+                        </div>
+                    </li>
+                    <li>
+                        <div>
+                            <h3 class="track-title">Hey Brother</h3>
+                            <p class="track-artist">Acicii</p>
+                        </div>
+                    </li>
+                    <li>
+                        <div>
+                            <h3 class="track-title">Polaroid</h3>
+                            <p class="track-artist">Jonas Blue <span class="supplement">&</span> Liam Payne <span class="supplement">&</span> Lennon Stella</p>
+                        </div>
+                    </li>
+                    <li>
+                        <div>
+                            <h3 class="track-title">Legends Never Die</h3>
+                            <p class="track-artist">League of Legends <span class="supplement">&</span> Against The Current</p>
+                        </div>
+                    </li>
+                    <li>
+                        <div>
+                            <h3 class="track-title">Beggin'</h3>
+                            <p class="track-artist">Måneskin</p>
+                        </div>
+                    </li>
+                    <li>
+                        <div>
+                            <h3 class="track-title new">Ms. Jackson</h3>
+                            <p class="track-artist">Outkast</p>
+                        </div>
+                    </li>
+                    <li>
+                        <div>
+                            <h3 class="track-title">Dangerous</h3>
+                            <p class="track-artist">David Guetta <span class="supplement">&</span> Sam Martin</p>
+                        </div>
+                    </li>
+                    <li>
+                        <div>
+                            <h3 class="track-title">Rockabye</h3>
+                            <p class="track-artist">Clean Bandit <span class="supplement">&</span> Sean Paul <span class="supplement">&</span> Anne-Marie</p>
+                        </div>
+                    </li>
+                    <li>
+                        <div>
+                            <h3 class="track-title">Yours</h3>
+                            <p class="track-artist">Andrew Huang</p>
+                        </div>
+                    </li>
+                    <li>
+                        <div>
+                            <h3 class="track-title">Someone To You</h3>
+                            <p class="track-artist">BANNERS</p>
+                        </div>
+                    </li>
+                    <li>
+                        <div>
+                            <h3 class="track-title new">Heat Waves</h3>
+                            <p class="track-artist">Glass Animals</p>
+                        </div>
+                    </li>
+                    <li>
+                        <div>
+                            <h3 class="track-title">Young, Wild and Free</h3>
+                            <p class="track-artist">Snoop Dogg <span class="supplement">&</span> Wiz Khalifa <span class="supplement">&</span> Bruno Mars</p>
+                        </div>
+                    </li>
+                    <li>
+                       <div>
+                            <h3 class="track-title">Champagne & Sunshine</h3>
+                            <p class="track-artist">PLVTINUM <span class="supplement">&</span> Tarro</p>
+                       </div>
+                    </li>
+                    <li>
+                        <div>
+                            <h3 class="track-title new">Remember</h3>
+                            <p class="track-artist">Becky Hill <span class="supplement">&</span> David Guetta</p>
+                        </div>
+                    </li>
+                    <li>
+                        <div>
+                            <h3 class="track-title">A Drop In The Ocean</h3>
+                            <p class="track-artist">Ron Pope</p>
+                        </div>
+                    </li>
+                </ol>
+            </article>
+        </main>
+
+        <footer class="page-footer">
+            &copy; <?php echo date("Y"); ?> Robin Boers. Webdesigner en Elixir programmeur sinds 2017.
+        </footer>
+    </body>
+</html>
