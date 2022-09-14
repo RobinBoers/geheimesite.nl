a1254a353e54e4b632b00e6c27bf528d71d32638
Robin Boers <robindev2019@outlook.com>
2022-09-14 16:36:30 +0200

    Update sitemap

---
+++ src/follow.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="nl">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Follow me — geheimesite.nl | Robin Boers</title>
+
+    <link rel="alternate stylesheet" href="/assets/css/plain.css" title="plain" />
+    <script src="/assets/js/plain-mode.js"></script>
+
+    <link rel="stylesheet" href="/assets/css/main.css" title="default" />
+    <script defer src="/assets/js/menu.js"></script>
+
+    <link rel="alternate" hreflang="en" href="https://geheimesite.nl" />
+</head>
+
+<body>
+    <a class="skip-nav" href="#content">Skip navigation</a>
+    <header class="pageheader">
+        <p class="title"><a href="/en">Robin Boers</a></p>
+
+        <img src="/assets/images/menu.svg" alt="menu button" class="menu-button" hidden />
+
+        <nav>
+            <ul class="menu">
+                <li><a href="/en">Home</a></li>
+                <li><a href="/en/about">About me</a></li>
+                <li><a href="/en/projects">Projects</a></li>
+                <li><a href="/en/books">Books</a></li>
+                <li><a href="https://blog.geheimesite.nl/en">Blog</a></li>
+            </ul>
+        </nav>
+    </header>
+
+    <main id="content" class="page-content">
+        <article>
+            <section>
+                <h1>Follow me</h1>
+                <p>
+                    I don't like social media. Actually, <a href="https://blog.geheimesite.nl/2021/12/social-media-wat-moeten-we-er-mee-aan.html">I hate it</a>. However, I do have a few <a href="https://en.wikipedia.org/wiki/RSS" target="_blank">RSS</a> feeds:
+                </p>
+
+                <ul>
+                    <li>
+                        <a href="https://blog.geheimesite.nl/index.xml">Webdevelopment-En-Meer</a>
+                        <small>(primary feed, dutch)</small>
+                    </li>
+                    <li>
+                        <a href="https://blog.geheimesite.nl/en/index.xml">ThatCrazyWebdev</a>
+                        <small>(less frequent posts, english)</small>
+                    </li>
+                    <li>
+                        <a href="https://micro.geheimesite.nl/feed">Robijntje's neolog</a>
+                        <small>(microblog, english)</small>
+                    </li>
+                    <li>
+                        <a href="https://blog.geheimesite.nl/stupid-codes/index.xml">Stupid Codes</a>
+                        <small>(feed with programming tips, inactive, english)</small>
+                    </li>
+                </ul>
+            </section>
+
+            <hr class="my-7 border-none">
+
+            <section>
+                <h2>What is RSS?</h2>
+
+                <p>RSS is like your facebook feed, but instead of an algorithm deciding what to show you, you choose which feeds to subscribe to. That means it is fully in your control: no ads, no snooping.</p>
+
+                <p>In order to read the feeds you need an <a href="https://en.wikipedia.org/wiki/News_aggregator" target="_blank">RSS reader</a>. I recommend either <a href="https://inoreader.com">Inoreader</a> or <a href="https://theoldreader.com" target="_blank">The Old Reader</a>.</p>
+
+                <p>I can explain how RSS works here, but I think Nicky Case does a way better job than I do, so please read <a href="https://ncase.me/rss">her explanation</a> instead :)</p>
+            </section>
+        </article>
+    </main>
+
+    <footer class=" page-footer">
+        &copy; <?php echo date("Y"); ?> Robin Boers. Webdesigner en Elixir programmeur sinds 2017.
+    </footer>
+</body>
+
+</html>
