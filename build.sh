c0fb237a0ffbafd3dc05968d4185c1ce9b32b625
Robin Boers <robindev2019@outlook.com>
2022-09-24 17:21:39 +0200

    Clean up folder structure

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # This script formats everything and builds the CSS files for me using tailwind.
 
-npx prettier --write .
 npx tailwindcss -c tailwind.config.js -i static/assets/css/source.css -o static/assets/css/main.css
 HUGO_UGLYURLS=true hugo --quiet 
 
 # Remove weirdness from hugo
 rm -rf public/nl
+npx prettier --write .
