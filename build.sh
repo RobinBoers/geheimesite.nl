3e1dc255018ae79f3b8c650b881b80a82d0bf9e1
Robin Boers <robindev2019@outlook.com>
2022-09-25 20:07:21 +0200

    Include images in build pipeline and introduce testbuild.sh

--- build.sh
+++ build.sh
@@ -8,10 +8,19 @@ echo "Building CSS..."
 npx tailwindcss -c tailwind.config.js -i src/assets/css/source.css -o src/assets/css/main.css > /dev/null 2>&1
 
 echo "Building site..."
-HUGO_UGLYURLS=true hugo --quiet 
+HUGO_UGLYURLS=true hugo --quiet
+
+echo "Copying over images..."
+# In the future I want to do compressing and 
+# optimizations here, but for now I'll just copy them over.
+cp -r ./src/assets/images ./dist/assets/images
 
 echo "Cleaning up build artifacts..."
 rm -rf dist/nl
+rm -rf dist/en/404.html
+mv dist/js dist/assets/js
+mv dist/css dist/assets/css
+mv dist/404.html dist/404.shtml
 
 echo "Validating build..."
 DEPLOY=true ./validate.sh || exit 1
---
+++ static/en/blog.php
@@ -0,0 +1,1 @@
+<?php header("Location: https://blog.geheimesite.nl/en") ?>
