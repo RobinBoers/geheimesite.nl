bb84340bd094dd5ca51a0cd78f878bf86057be37
Robin Boers <robindev2019@outlook.com>
2022-10-03 18:01:48 +0200

    Force code blocks in CSS builds

--- static/blog/index.php
+++ static/blog/index.php
@@ -1,2 +1,2 @@
 <?php header("Location: https://blog.geheimesite.nl") ?>
-<!-- Trick tailwind to include footnotes when building -->
+<!-- Trick tailwind to include footnotes and highlight code blocks when building. This comment containing those words makes it happen :) -->
