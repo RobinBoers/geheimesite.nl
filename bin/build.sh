dc8b52bb006266a034dac1405a0ffe81fd95aedd
Robin Boers <robindev2019@outlook.com>
2023-01-21 16:01:31 +0100

    Fix tailwind errors

--- bin/build.sh
+++ bin/build.sh
@@ -8,7 +8,7 @@ echo "Cleaning cache..."
 rm -rf dist
 
 echo "Building CSS..."
-npx tailwindcss -c tailwind.config.js -i src/assets/css/source.css -o src/assets/css/main.css --minify > /dev/null 2>&1
+npx tailwindcss -c tailwind.config.js -i src/assets/css/source.css -o src/assets/css/main.css --minify > /dev/null 2>&1 || exit 1
 
 if ! $VALIDATE; then
     echo "Building site..."
