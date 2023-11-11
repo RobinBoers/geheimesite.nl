9cfd9d329a9bd3d0182943ec09f326017e766d7f
Robin Boers <robindev2019@outlook.com>
2023-11-11 11:09:03 +0100

    Setup RSS feeds
    I now have three RSS feeds:
    
    - Dutch only
    - English only
    - Bilingual
    
    I set it up thru a system of code sharing between templates (see chop
    commit). I also updated the links on the follow pages.

--- .ci/deploy.sh
+++ .ci/deploy.sh
@@ -1,8 +1,22 @@
 #!/usr/bin/env fish
 
 bix build
+
+# Upload site files
 rsync -ciavuz --delete dist/gemini/ robin@dupunkto.org:pub/gem
 rsync -ciavuz --delete dist/web/ robin@dupunkto.org:pub/web
-rsync -ciavuz dist/rss/ robin@dupunkto.org:pub/web
-rsync -ciavuz dist/rss/ robin@dupunkto.org:pub/gem
+
+# Upload bilingual RSS feed
+rsync -ciavuz --delete dist/rss/index.xml robin@dupunkto.org:pub/web/index.xml
+rsync -ciavuz --delete dist/rss/index.xml robin@dupunkto.org:pub/gem/index.xml
+
+# Upload single-language RSS feeds
+for directory in dist/rss-*
+    set language (echo $directory | cut -d'-' -f2)
+    
+    rsync -ciavuz dist/rss-$language/index.xml robin@dupunkto.org:pub/web/index-$language.xml
+    rsync -ciavuz dist/rss-$language/index.xml robin@dupunkto.org:pub/gem/index-$language.xml
+end
+
+
 
