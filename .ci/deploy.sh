1eccac841665ca81e0d46ae39ef166ff9face539
Robin Boers <robindev2019@outlook.com>
2023-11-11 22:23:49 +0100

    Improve deploy scripts

--- .ci/deploy.sh
+++ .ci/deploy.sh
@@ -1,22 +1,30 @@
 #!/usr/bin/env fish
 
+set ignore_file .ci/.deployignore
+
+set remote robin@dupunkto.org:pub
+# set remote geheimesite.nl:domains/geheimesite.nl
+
+set gem_dir gem
+set web_dir web
+# set gem_dir public_gemini
+# set web_dir public_html
+
 bix build
 
 # Upload site files
-rsync -ciavuz --delete dist/gemini/ robin@dupunkto.org:pub/gem
-rsync -ciavuz --delete dist/web/ robin@dupunkto.org:pub/web
+rsync -ciavuz --delete --exclude-from=$ignore_file dist/gemini/ "$remote/$gem_dir"
+rsync -ciavuz --delete --exclude-from=$ignore_file dist/web/ "$remote/$web_dir"
 
 # Upload bilingual RSS feed
-rsync -ciavuz --delete dist/rss/index.xml robin@dupunkto.org:pub/web/index.xml
-rsync -ciavuz --delete dist/rss/index.xml robin@dupunkto.org:pub/gem/index.xml
+rsync -ciavuz --delete dist/rss/index.xml "$remote/$gem_dir/index.xml"
+rsync -ciavuz --delete dist/rss/index.xml "$remote/$web_dir/index.xml"
 
 # Upload single-language RSS feeds
 for directory in dist/rss-*
     set language (echo $directory | cut -d'-' -f2)
-    
-    rsync -ciavuz dist/rss-$language/index.xml robin@dupunkto.org:pub/web/index-$language.xml
-    rsync -ciavuz dist/rss-$language/index.xml robin@dupunkto.org:pub/gem/index-$language.xml
+   
+    rsync -ciavuz dist/rss-$language/index.xml "$remote/$gem_dir/index-$language.xml"
+    rsync -ciavuz dist/rss-$language/index.xml "$remote/$web_dir/index-$language.xml"
 end
-
-
 
