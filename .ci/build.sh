2c48e3a345457fb12e2b94294917092ee9c28f86
Robin Boers <robindev2019@outlook.com>
2023-11-12 21:14:52 +0100

    Improve publishing workflow

--- .ci/build.sh
+++ .ci/build.sh
@@ -1,5 +1,15 @@
 #!/usr/bin/env fish
 
+set base https://dupunkto.org
+# set base https://geheimesite.nl
+
+set prefix "/~robin"
+
+cp .ci/config.template config.yaml
+echo >> config.yaml
+echo "  baseurl: $base" >> config.yaml
+echo "  prefix: $prefix" >> config.yaml
+
 ~/projects/personal/chop/chop.js
 
 echo
