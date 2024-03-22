af16f635f1d16baf164235d46b4c448de08d3b7d
Robin Boers <robindev2019@outlook.com>
2024-03-22 23:01:32 +0100

    Very very WIP

--- src/r/place.php
+++ src/r/place.php
@@ -55,9 +55,10 @@ function save_canvas($canvas) {
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <meta name="color-scheme" content="dark light">
 
     <title>r/place</title>
+
+    <meta name="color-scheme" content="dark light">
 
     <style>
       body,
