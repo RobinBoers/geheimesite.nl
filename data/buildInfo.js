2b7910bb391e6a78ea86d059c4b7174b40013061
Robin <robindev2019@outlook.com>
2024-10-27 13:54:37 +0100

    Setup footer containing buildInfo

---
+++ data/buildInfo.js
@@ -0,0 +1,34 @@
+import { $ } from "bun";
+
+const SELF = "package.json";
+const ELEVENTY = "node_modules/@11ty/eleventy/package.json";
+
+const formatDateTime = (dt, tz) =>
+  new Intl.DateTimeFormat("en-US", {
+    month: "long",
+    day: "numeric",
+    year: "numeric",
+    timeZone: tz,
+  }).format(dt);
+
+const generateBuildInfo = async () => {
+  const now = new Date();
+  const tz = "CET";
+
+  const buildVersion = (await Bun.file(SELF).json()).version;
+  const generatorVersion = (await Bun.file(ELEVENTY).json()).version;
+  const commitHash = (await $`git rev-parse --short HEAD`.text()).trim();
+
+  return {
+    buildVersion,
+    generatorVersion,
+    commitHash,
+    buildDate: {
+      timestamp: now.getTime(),
+      iso: now.toISOString(),
+      human: `${formatDateTime(now, tz)}`,
+    },
+  };
+};
+
+export default generateBuildInfo;
