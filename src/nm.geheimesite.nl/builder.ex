8a027f977d3bad584eaeb26f526ecb343b75cc54
Robin <robindev2019@outlook.com>
2026-08-25 21:31:10 +0200

    Redesign netmap

--- src/nm.geheimesite.nl/builder.ex
+++ src/nm.geheimesite.nl/builder.ex
@@ -4,13 +4,14 @@ defmodule Netmap do
 
   copy_through "domains.json"
 
+  @netmap Application.app_dir(:vygotsky, "priv/netmap.json")
+  @external_resource @netmap
+
   for path <- glob("*.html.heex") do
     @external_resource path
     @dest target(path)
 
-    [embed] = register_dependencies!(path)
-
-    if outdated?(@dest, path) or outdated?(@dest, embed) do
+    if outdated?(@dest, path) or outdated?(@dest, @netmap) do
       File.write!(@dest, VEEx.render_template!(path))
     end
   end
